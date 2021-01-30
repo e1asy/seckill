ab0378968e8774824be012d63661c5446ccbd0f3
tink2r <2511384642@qq.com>
2021-01-30 20:06:23 +0800

    项目初始化

---
+++ src/test/java/com/ncepu/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncepu.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
